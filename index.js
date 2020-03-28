8b6bbad536cbfd0f54e9bffbab1d2bb0e2867a3b
Thiago Denny <tdenny@daitan.com>
2020-03-28 20:43:43 -0300

    Change messages

--- index.js
+++ index.js
@@ -3,16 +3,21 @@ const urlName = process.argv[3];
 const fs = require('fs');
 const axios = require('axios');
 const readStream = fs.createReadStream(`./${fileName}`,{ highWaterMark: 1 * 1024, encoding: 'base64' });
-let count = 0;
 
+let count = 0;
 readStream.on('data', async function(chunk) {
-    console.log(count);
     const result = await axios({
       method: 'post',
       url: `http://dontpad.com/${urlName}/${count++}`,
       contentType: 'application/x-www-form-urlencoded;charset=UTF-8',
       data: `text=${chunk}`
     })
-}).on('end', function() {
-    console.log('OK');
+    .then(response => {
+      const { url } = response.config;
+      console.log(url);
+    });
+});
+
+process.on("exit", function(){
+    console.log(`Done! 0 -> ${count}`);
 });

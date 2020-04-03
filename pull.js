7a79c20897f98f52ec13ab42d09b54524a22a105
Thiago Denny <thiagodenny@outlook.com>
2020-04-02 21:25:55 -0300

    Fix eslint problems

--- pull.js
+++ pull.js
@@ -6,23 +6,21 @@ const axios = require('axios');
 
 const writeStream = fs.createWriteStream(`./${fileName}`, { flags: 'a' });
 
-const getFile = async count => {
-  const url = `http://dontpad.com/${urlName}/${count}`
+const getFile = async (count) => {
+  const url = `http://dontpad.com/${urlName}/${count}`;
   axios({
     method: 'get',
-    url: url
-  }).then(({ data }) => {
-    return data.substring(
-      data.search(`<textarea id="text">`) + 20,
-      data.search(`</textarea>`)
-    );
-  }).then(result => {
-    if (result != '') {
+    url,
+  }).then(({ data }) => data.substring(
+    data.search('<textarea id="text">') + 20,
+    data.search('</textarea>'),
+  )).then((result) => {
+    if (result !== '') {
       console.log(url);
       writeStream.write(result);
-      getFile(++count);
+      getFile(count + 1);
     }
-  })
-}
+  });
+};
 
-getFile(0)
+getFile(0);
--- push.js
+++ push.js
@@ -2,22 +2,23 @@ const fileName = process.argv[2];
 const urlName = process.argv[3];
 const fs = require('fs');
 const axios = require('axios');
-const readStream = fs.createReadStream(`./${fileName}`,{ highWaterMark: 1 * 1024, encoding: 'base64' });
+
+const readStream = fs.createReadStream(`./${fileName}`, { highWaterMark: 1 * 1024, encoding: 'base64' });
 
 let count = 0;
-readStream.on('data', async function(chunk) {
-  const result = await axios({
+readStream.on('data', async (chunk) => {
+  await axios({
     method: 'post',
     url: `http://dontpad.com/${urlName}/${count++}`,
     contentType: 'application/x-www-form-urlencoded;charset=UTF-8',
-    data: `text=${chunk}`
+    data: `text=${chunk}`,
   })
-  .then(response => {
-    const { url } = response.config;
-    console.log(url);
-  });
+    .then((response) => {
+      const { url } = response.config;
+      console.log(url);
+    });
 });
 
-process.on("exit", function(){
+process.on('exit', () => {
   console.log(`Done! 0 -> ${count}`);
 });
